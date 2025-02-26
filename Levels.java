ed31d55121b489a231589fbc82cd72573b476b39
EminDri <135602559+EminDri@users.noreply.github.com>
2025-02-26 17:25:06 +0000

    Add files via upload

---
+++ Levels.java
@@ -0,0 +1,64 @@
+
+/**
+ * Write a description of class Levels here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Levels
+{
+    // instance variables - replace the example below with your own
+    public int points = 0;
+    private int currentLevel = 0;
+    private int nextLevel;
+    private int LEVEL_UP_POINTS = 100;
+
+    /**
+     * Constructor for objects of class Levels
+     */
+    public Levels()
+    {
+        nextLevel = currentLevel + 1;
+    }
+
+    //need to add correctAnswer call
+    public void addPoints(int xp) {
+        if (!maxLevel()) {
+            points += xp;
+            levelUp();
+        } else {
+            points = points;
+        }
+    }
+
+    private void levelUp() {
+        if (getPoints() > LEVEL_UP_POINTS && !maxLevel()) {
+            currentLevel = nextLevel;
+            nextLevel = currentLevel + 1;
+            points = 0;
+            LEVEL_UP_POINTS += 25;
+        } else if(maxLevel()) {
+            System.out.println("Max Level");
+        }
+    }
+
+    private int getPoints() {
+        return points;
+    }
+
+    private int getLevel() {
+        return currentLevel;
+    }
+
+    private void showLevel () {
+        if (maxLevel()) {
+            System.out.println("Max Level");
+        }else {
+            System.out.println("Level: " + currentLevel);
+        }
+    }
+
+    private boolean maxLevel () {
+        return getLevel() >= 100;
+    }
+}
